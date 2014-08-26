b60fa517a1b83b46d1ce3b24c49586b64f4ddd82
yangxinyi <xin3336019@sina.com>
2014-08-26 22:06:54 +0800

    新WEB工程初始化完成

---
+++ src/main/java/com/map/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.map.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * @author yangxinyi
+ * 娴嬭瘯鐢�
+ */
+@Controller
+@RequestMapping(value="")
+public class TestController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView("/status");
+		modelAndView.addObject("status", "succ full");
+		LOG.info("run success");
+		return modelAndView;
+	}
+	
+}
