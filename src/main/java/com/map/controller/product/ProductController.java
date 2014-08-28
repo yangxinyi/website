040d629de2875ca146a5aac2add15a31989c9d61
yangxinyi <xin3336019@sina.com>
2014-08-28 16:18:25 +0800

    localhost/product/test页面测试保存商品信息完成

---
+++ src/main/java/com/map/controller/product/ProductController.java
@@ -0,0 +1,44 @@
+package com.map.controller.product;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.map.model.product.Product;
+import com.map.service.product.ProductService;
+
+/**
+ * 
+ * @author yangxinyi
+ * 娴嬭瘯鐢�
+ */
+@Controller
+@RequestMapping(value="product")
+public class ProductController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ProductController.class);
+	
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public ModelAndView test() {
+		ModelAndView modelAndView = new ModelAndView("/product_index");
+		Product product = new Product();
+		product.setName("页面测试");
+		product.setBrandId(5L);
+		product.setPath("tmp");
+		product.setProductGenreId(2L);
+		product.setPrice(new BigDecimal(0.5));
+		productService.save(product);
+		modelAndView.addObject("product_id", product.getId());
+		return modelAndView;
+	}
+	
+}
---
+++ src/main/webapp/page/product_index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>product id ${product_id }</h2>
+</body>
+</html>
